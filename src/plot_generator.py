b4e0a2798a3fe09c1cbaa0c852e3130acad060d5
Zach Bochanski <ztbochanski@gmail.com>
2024-04-02 00:46:29 -0700

    add plot generator class

---
+++ src/plot_generator.py
@@ -0,0 +1,28 @@
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class PlotGenerator:
+    """Generates and saves plots based on the processed data."""
+
+    @staticmethod
+    def generate(data_frames: pd.DataFrame, folder_path: str, mouse_id: str) -> None:
+        """Generates and saves a plot for the merged data using Seaborn.
+
+        Args:
+            data_frames: The merged DataFrame containing all data to be plotted.
+            folder_path: The path to the folder where the plot will be saved.
+            mouse_id: The ID of the mouse, used for naming the plot file.
+        """
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=data_frames, x='BinCal', y='Perc_Area', hue='Timepoint', marker='o', style='Timepoint', dashes=False)
+        
+        plt.xlabel('BinCal')
+        plt.ylabel('Perc_Area')
+        plt.title(f'Mouse ID: {mouse_id}')
+        plt.legend(title='Timepoint')
+        plt.savefig(f'plots/{mouse_id}_plot.png')
+        plt.close()
